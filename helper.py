b737fefc9c9b1770fe382597562d1465069c8fe2
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-05-31 11:17:57 +0530

    final json made

--- helper.py
+++ helper.py
@@ -3,17 +3,20 @@ import json
 from pprint import pprint
 import requests
 
+
 def getSoup(link):
     req = requests.get(link)
     html = req.content
     soup = BeautifulSoup(html, "html.parser")
     return soup
 
+
 def getSoup_headers(link, headers):
     req = requests.get(link, headers=headers)
     html = req.content
     soup = BeautifulSoup(html, "html.parser")
     return soup
+
 
 def get_page_games(link):
     soup = getSoup(link)
@@ -26,14 +29,16 @@ def get_page_games(link):
         print(game_id)
     return game_ids
 
+
 def get_price(game_id):
-    link = "https://gg.deals/us/games/offers/{}/?GameOffersSearch%5BloadMore%5D=1".format(game_id)
+    link = "https://gg.deals/us/games/offers/{}/?GameOffersSearch%5BloadMore%5D=1".format(
+        game_id)
     headers = {"X-Requested-With": "XMLHttpRequest"}
-    soup = getSoup_headers(link, headers = headers)
+    soup = getSoup_headers(link, headers=headers)
     #CDKEYS, ENEBA, MMOGA
     keyshops = soup.findAll("div", {"class": "game-deals-item"})
     ret = ["", "", ""]
-    name = soup.find("div", {"class":"ellipsis title"})['title']
+    name = soup.find("div", {"class": "ellipsis title"})['title']
     ret[0] = name
     for keyshop in keyshops:
         shop = keyshop.find("div", {"class": "deal-hoverable action-wrap"})
@@ -46,15 +51,17 @@ def get_price(game_id):
             break
     return ret
 
+
 def get_games():
     games = {}
     games["game_ids"] = []
     for i in range(1, 41):
         link = "https://gg.deals/games/?page={}".format(i)
         games["game_ids"].extend(get_page_games(link))
-    
+
     with open("games.json", "w") as f:
         json.dump(games, f)
+
 
 def get_prices():
     game_details = {}
@@ -72,7 +79,43 @@ def get_prices():
         json.dump(game_details, f)
 
 
+def make_final():
+    with open("game_details.json") as f:
+        details = json.load(f)
+    with open("final.json") as f:
+        final_json = json.load(f)
+
+    ctr = final_json['ctr']
+    final = final_json['details']
+    for key_details, value_details in details.items():
+        flag = 0
+        for (index, value_final) in enumerate(final):
+            if key_details == value_final[4]:
+                flag = 1
+                if not value_details[1] == '':
+                    row = value_final
+                    row[2] = value_details[1]
+                    row[3] = value_details[2]
+                    final_json['details'][index] = row
+                break
+        if flag == 0:
+            if not value_details[1] == '':
+                row = ['', '', '', '', '']
+                row[0] = ctr
+                row[1] = value_details[0]
+                row[2] = value_details[1]
+                row[3] = value_details[2]
+                row[4] = key_details
+                final_json['details'].append(row)
+                ctr = ctr+1
+
+    final_json['ctr'] = ctr
+    with open("final.json", "w") as f:
+        json.dump(final_json, f)
+
+
 def driver():
-    # get_games()
+    get_games()
     get_prices()
     # print(get_price("80011"))
+    make_final()
